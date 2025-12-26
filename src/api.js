9cbdf1cc203d0ce87362ee9d0949fcb19b36ca13
Shirsendu Pramanik <pramanikshirsendu07@gmail.com>
2025-12-27 05:08:58 +0530

    Update API base URL to new production endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://proactive-dedication-production-c4d4.up.railway.app/api",
+  baseURL: "https://studentforn-backend-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
